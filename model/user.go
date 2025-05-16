25a698359c9002165d8db93e888c675398265c82
Teppei Taguchi <tetzng.tt@gmail.com>
2025-05-17 02:06:47 +0900

    feat: add signup

--- model/user.go
+++ model/user.go
@@ -4,7 +4,7 @@ import "time"
 
 type User struct {
 	Id        uint      `json:"id" gorm:"primaryKey"`
-	Name      string    `json:"name" gorm:"unique"`
+	Name      string    `json:"name"`
 	Email     string    `json:"email" gorm:"unique"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
